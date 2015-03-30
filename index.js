0bec7a833a405c7c18f4fc30bea8bd8a31754456
macbook <macbook@macbooks-MacBook-Air.local>
2015-03-30 10:59:30 +0300

    Getting files from *.pass directory

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-var exec = require('child_process').exec,
-    child;
+exports.createPassbook = function(type, resorces, keys, password){
+	var passbook = require('./lib/index.js');
+	var fs = require("fs");
+	var obj;
+	fs.readFile(resources+'pass.json', 'utf8', function (err, data) {
+  		if (err) throw err;
+  		console.log('pass.json file redaded successfully');
+  		obj = JSON.parse(data);
+  		var template = passbook.createTemplate(type, {
+		passTypeIdentifier: obj.passTypeIdentifier,
+		teamIdentifier:     obj.teamIdentifier,
+		organizationName:   obj.organizationName
+		}, {
+		certs: {
+		  wwdr: keys+'/wwdr.pem',
+		  pass: keys+'/Certificates.pem', // pem with certificate and private key
+		  password: password // pass phrase for the pass_cert.pem file
+		}
+		})
+		var pass = template.createPass(obj)
+		pass.pipe(fs.createWriteStream('pass.pkpass'))
+	});
+}
 
-child = exec('./bin/signpass -p Lollipop.pass',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
+
+
+
+exports.prepareKeys = function(keys){
+	console.log("Preparing keys in directory "+ keys);
+	var exec = require('child_process').exec,
+	    child;
+	child = exec('node node-passbook prepare-keys -p '+keys,
+	  function (error, stdout, stderr) {
+	    console.log('stdout: ' + stdout);
+	    console.log('stderr: ' + stderr);
+	    if (error !== null) {
+	      console.log('exec error: ' + error);
+	    }
+	});
+	console.log("Keys prepared");
+}
+
+
+
